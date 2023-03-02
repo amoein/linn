57172574f4e77f0389843ad790fc03089f7efe60
Moein Abbasi <amoein.work@gmail.com>
2023-03-02 18:13:03 +0300

    fix test and add option

--- src/linn.erl
+++ src/linn.erl
@@ -1,11 +1,16 @@
 -module(linn).
--author("info@moein").
+
+-author("amoein").
+
+-include("linn.hrl").
 
 %% API
--export([add_pool/4, get_process/1]).
+-export([add_pool/1, get_process/1]).
 
-add_pool(Id, M, F, Count) ->
-    linn_sup:add_pool(Id, M, F, Count).
+-spec add_pool(Option :: option()) -> ok.
+add_pool(Option) ->
+    linn_sup:add_pool(Option).
 
+-spec get_process(Id :: atom()) -> {ok, Pid :: pid()} | {error, not_found}.
 get_process(Id) ->
     linn_pool:get(Id).
--- test/linn_pool_SUITE.erl
+++ test/linn_pool_SUITE.erl
@@ -1,37 +1,47 @@
 -module(linn_pool_SUITE).
--author("aleyandro").
+
+-author("amoein").
 
 -include_lib("common_test/include/ct.hrl").
+
+-include("linn.hrl").
+
 %% Test server callbacks
 -export([suite/0, all/0, init_per_suite/1, end_per_suite/1]).
 
 %% Test cases
 -export([pool/1, multi/1]).
 
-suite() ->
-    [].
+suite() -> [].
 
 init_per_suite(Config) ->
     application:start(linn),
+    Option1 = #option{
+        pool_id = test1,
+        handler_module = linn_test_server,
+        handler_func = start_link,
+        handler_func_arity = 0,
+        process_count = 10
+    },
+    Option2 = #option{
+        pool_id = test2,
+        handler_module = linn_test_server,
+        handler_func = start_link,
+        handler_func_arity = 0,
+        process_count = 100
+    },
+    linn:add_pool(Option1),
+    [{option1, Option1}, {option2, Option2} | Config].
 
-    Config.
+end_per_suite(_Config) -> ok.
 
-end_per_suite(_Config) ->
-    ok.
-
-all() ->
-    [
-        pool,
-        multi
-    ].
+all() -> [pool, multi].
 
 pool(_Config) ->
-    linn:add_pool(test, linn_test_server, start_link, 10),
-    Res = linn:get_process(test),
+    Res = linn:get_process(test1),
     erlang:is_pid(Res).
 
 multi(_Config) ->
-    linn:add_pool(test2, linn_test_server, start_link, 10),
     T1 = erlang:system_time(millisecond),
 
     L = [
@@ -39,10 +49,10 @@ multi(_Config) ->
             Res = linn:get_process(test2),
             erlang:is_pid(Res)
         end
-     || _ <- lists:seq(1, 1000000)
+     || _ <- lists:seq(1, 1_000_000)
     ],
 
     T2 = erlang:system_time(millisecond),
-    [ct:print("Result ~p", [(T2 - T1)])],
+    [?LOG_INFO("Result ~p", [(T2 - T1)])],
 
     not lists:member(false, L).
